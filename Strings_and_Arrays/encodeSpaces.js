467a1d97124f016ad31139cb5e2c58bce1064009
Ahmed Modan <ahmed.modan@gmail.com>
2016-06-19 00:49:31 -0700

    [ctci] encodeSpaces ctci solution with no splice

--- Strings_and_Arrays/encodeSpaces.js
+++ Strings_and_Arrays/encodeSpaces.js
@@ -1,10 +1,38 @@
+// function encodeSpaces(url) {
+//   if (!url || url.length === 0) return url;
+//   const encoded = url.split('');
+//   for (let i = 0; i < encoded.length; i++) {
+//     if (encoded[i] === ' ') {
+//       encoded.splice(i, 1, '%','2','0')
+//     }
+//   }
+//   return encoded.join('');
+// }
+
+
+// expect array of characters to be passed in
+// no array.splice
 function encodeSpaces(url) {
   if (!url || url.length === 0) return url;
-  const encoded = url.split('');
-  for (let i = 0; i < encoded.length; i++) {
-    if (encoded[i] === ' ') {
-      encoded.splice(i, 1, '%','2','0')
+
+  let spaces = 0;
+
+  for (let i = 0; i < url.length; i++) {
+    if (url[i] === ' ') {
+      spaces++;
     }
   }
-  return encoded.join('');
+
+  const newLength = url.length - 1 + (spaces * 2);
+  for (let i = url.length - 1, j= newLength; i >= 0 && j > i; i-- , j--) {
+    if (url[i] === ' ') {
+      url[j] = '0';
+      url[--j] = '2';
+      url[--j] = '%';
+    } else {
+      url[j] = url[i];
+    }
+  }
+
+  return url;
 }
